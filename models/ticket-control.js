f8cd8626a993787869b9dfdf5b9495cf17f543e3
Sebastian Carrillo <scarrilloh77@gmail.com>
2022-08-27 20:12:54 -0500

    Modelo - Siguiente y atender nuevo ticket

--- models/ticket-control.js
+++ models/ticket-control.js
@@ -1,6 +1,13 @@
 const path = require('path');
 const fs = require('fs');
 
+class Ticket {
+  constructor(numero, escritorio) {
+    this.numero = '';
+    this.escritorio = '';
+  }
+}
+
 class TicketControl {
   constructor() {
     this.ultimo = 0;
@@ -19,6 +26,7 @@ class TicketControl {
       ultimos4: this.ultimos4,
     };
   }
+
   init() {
     const { hoy, tickets, ultimo, ultimos4 } = require('../db/data.json');
     if (hoy === this.hoy) {
@@ -30,9 +38,39 @@ class TicketControl {
       this.guardarDB();
     }
   }
+
   guardarDB() {
     const dbPath = path.join(__dirname, '../db/data.json');
     fs.writeFileSync(dbPath, JSON.stringify(this.toJson));
+  }
+
+  siguiente() {
+    this.ultimo += 1;
+    const ticket = new Ticket(this.ultimo, null);
+    this.tickets.push(ticket);
+
+    this.guardarDB();
+    return 'Ticket ' + ticket.numero;
+  }
+
+  atenderTicket(escritorio) {
+    // No tenemos tickets
+    if (this.tickets.length === 0) {
+      return null;
+    }
+
+    const ticket = this.tickets.shift(); // this.tickets[0];
+    ticket.escritorio = escritorio;
+
+    this.ultimos4.unshift(ticket);
+
+    if (this.ultimos4.length > 4) {
+      this.ultimos4.splice(-1, 1); // Cortar el ultimo!
+    }
+
+    this.guardarDB();
+
+    return ticket;
   }
 }
 
