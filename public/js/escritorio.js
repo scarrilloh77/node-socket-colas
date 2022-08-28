ed0d504c5d22609878fb0efb992678fa2d784001
Sebastian Carrillo <scarrilloh77@gmail.com>
2022-08-28 00:51:17 -0500

    Preparar pantalla de escritorio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,32 @@
+const lblEscritorio = document.querySelector('h1'); // Primer h1 que encuentre!
+const btnAtender = document.querySelector('button');
 
+const searchParams = new URLSearchParams(window.location.search);
 
-console.log('Escritorio HTML');
+if (!searchParams.has('escritorio')) {
+  window.location = 'index.html';
+  throw new Error('El escritorio es obligatorio!'); // No es return porque no es una función
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio;
+
+const socket = io();
+
+socket.on('connect', () => {
+  btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+  btnAtender.disabled = true;
+});
+
+socket.on('ultimo-ticket', (ultimo) => {
+  // lblNuevoTicket.innerText = `Ticket ${ultimo}`;
+});
+
+btnAtender.addEventListener('click', () => {
+  // socket.emit('siguiente-ticket', null, (ticket) => {
+  //   lblNuevoTicket.innerText = ticket;
+  // });
+});
